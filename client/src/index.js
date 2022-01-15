14ee6da02dcfb5f24f18dcf5b5202a5f1c3ae632
Brandon Nathan Marks <bmarks7@uwo.ca>
2022-01-15 11:52:22 -0500

    merging HomePage into Development

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import Home from "./Pages/Home";
 import Progress from "./Pages/Progress";
 
 ReactDOM.render(
