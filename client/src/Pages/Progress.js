d2c0b5f5de445064a7951987aa9d191f3c2cc4c6
Brandon Nathan Marks <bmarks7@uwo.ca>
2022-01-15 15:15:15 -0500

    added graphs and table to progress page

--- client/src/Pages/Progress.js
+++ client/src/Pages/Progress.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Graph from '../components/Graphs';
+import Table from '../components/Table.tsx'
+import '../Styles/Progress.scss'
 
 export default class Progress extends Component {
     render() {
         return (
-            <div>
-                <h1>Progress</h1>
+            <div className='progressPage'>
+                <div className="progressPage__progress">
+                    <p className='progressPage__progressHeader'>Track Your Progress</p>
+                    <div className="progressPage__progressCharts">
+                        <Graph/>
+                    </div>
+                </div>
+
+                <div className="progressPage__pastSpeeches">
+                    <p className="progressPage__pastSpeechesHeader">Past Speeches</p>
+                    <div className="progressPage__pastSpeechesTable">
+                        <Table />
+                    </div>
+                </div>
             </div>
         )
     }
