98609bb2cab61125f0bbe0c56bccda57bb366a47
Leo Spallino <spallino@icloud.com>
2022-01-16 11:13:23 -0500

    Fixing warnings and protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import "./App.scss";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./Pages/Home";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Progress from './Pages/Progress'
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import Progress from "./Pages/Progress";
 import SpeechDetails from "./Pages/SpeechDetails";
-import {useState} from 'react'
 
-import {useAuth0} from '@auth0/auth0-react'
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { isLoading } = useAuth0();
