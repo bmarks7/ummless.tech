52d3b66e3646db2a1a85e4d31391e5c69137306b
Leo Spallino <spallino@icloud.com>
2022-01-16 01:57:18 -0500

    flix home page layout

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,13 +1,22 @@
 import FileUpload from "../components/FileUpload";
 import InDepthContainer from "../components/InDepthContainer";
 import SpeechSummary from "../components/SpeechSummary";
+import "../Styles/HomePage.scss";
 
 const Home = () => {
   return (
-    <div>
-      <FileUpload />
-      <SpeechSummary />
-      <InDepthContainer />
+    <div className="pageContainer">
+      <div className="left">
+        <div className="upload">
+          <FileUpload />
+        </div>
+        <div className="speechSummary">
+          <SpeechSummary />
+        </div>
+      </div>
+      <div className="right">
+        <InDepthContainer />
+      </div>
     </div>
   );
 };
