133f9370f515a0cba805ab323c5cc40e2a4435b2
Leo Spallino <spallino@icloud.com>
2022-01-15 20:47:56 -0500

    Fix no table body warning

--- client/src/components/SpeechSummaryTable.jsx
+++ client/src/components/SpeechSummaryTable.jsx
@@ -7,50 +7,54 @@ const SpeechSummaryTable = ({ data }) => {
 
   return (
     <table>
-      <tr>
-        <td>
-          <h3 className="title">Filler Words per Minute</h3>
-        </td>
-        <td
-          className={data.fillerWPM < 3 ? "data__positive" : "data__negative"}
-        >
-          {data.fillerWPM}
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h3 className="title">Speaking Speed</h3>
-        </td>
-        <td
-          className={
-            data.speed > 120 && data.speed < 140
-              ? "data__positive"
-              : "data__negative"
-          }
-        >
-          {data.speed}
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h3 className="title">Average Sentiment</h3>
-        </td>
-        <td
-          className={
-            data.averageSentiment > 0 ? "data__positive" : "data__negative"
-          }
-        >
-          {data.averageSentiment}
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h2 className="title">Score</h2>
-        </td>
-        <td className={data.score >= 70 ? "data__positive" : "data__negative"}>
-          <h2>{data.score}</h2>
-        </td>
-      </tr>
+      <tbody>
+        <tr>
+          <td>
+            <h3 className="title">Filler Words per Minute</h3>
+          </td>
+          <td
+            className={data.fillerWPM < 3 ? "data__positive" : "data__negative"}
+          >
+            {data.fillerWPM}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h3 className="title">Speaking Speed</h3>
+          </td>
+          <td
+            className={
+              data.speed > 120 && data.speed < 140
+                ? "data__positive"
+                : "data__negative"
+            }
+          >
+            {data.speed}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h3 className="title">Average Sentiment</h3>
+          </td>
+          <td
+            className={
+              data.averageSentiment > 0 ? "data__positive" : "data__negative"
+            }
+          >
+            {data.averageSentiment}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h2 className="title">Score</h2>
+          </td>
+          <td
+            className={data.score >= 70 ? "data__positive" : "data__negative"}
+          >
+            <h2>{data.score}</h2>
+          </td>
+        </tr>
+      </tbody>
     </table>
   );
 };
